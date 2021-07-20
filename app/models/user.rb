73211ea986b99e9c3bfbad1e0eb04868ce906eff
Mustafa I <milikkan@yahoo.com>
2021-07-21 00:36:58 +0300

    Add login, signup and edit password

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@
 class User < ApplicationRecord
 	has_secure_password
 
-	validates :email, presence: true, format: {with: URI::MailTo::EMAIL_REGEXP, message: "Invalid email address provided"}
+	validates :email, presence: true, format: {with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address"}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
 	root to: 'main#index'
+
 	get 'about', to: 'about#index'
+
+	get 'signup', to: 'registrations#new'
+	post 'signup', to: 'registrations#create'
+
+	get 'signin', to: 'sessions#new'
+	post 'signin', to: 'sessions#create'
+
+	delete 'logout', to: 'sessions#destroy'
+
+	get 'password', to: 'passwords#edit', as: :edit_password
+	patch 'password', to: 'passwords#update'
 end
